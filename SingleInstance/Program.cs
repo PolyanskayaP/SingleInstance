199ec8618522d517dde456f72f0f1a318d96f62e
Полина Полянская <ppolina353@yandex.ru>
2022-01-21 20:31:18 +0300

    Добавьте файлы проекта.

---
+++ SingleInstance/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Mutex oneMutex = null;
+        const String MutexName = "RUNMEONCE";
+        try //пытаемся открыть мьютекс
+        {
+            oneMutex = Mutex.OpenExisting(MutexName);
+        }
+        catch(WaitHandleCannotBeOpenedException)
+        {
+            //определяем, сущ ли мьютекс с таким именем
+        }
+        // Создаем его, если он не существует 
+        if (oneMutex == null)
+        {
+            oneMutex = new Mutex(true, MutexName);
+        }
+        else
+        {
+            // Закрываем Mutex и выходим из приложения,
+            // так как разрешается запуск только одного его экземпляра
+            oneMutex.Close();
+            return;
+        }
+    }
+}
